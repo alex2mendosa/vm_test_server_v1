7b117c16a5fa379af5da830e4e0310d79fa3c6ae
Alexandr Cecetov <66001736+alex2mendosa@users.noreply.github.com>
2025-10-03 19:15:39 +0300

    Add files via upload

---
+++ 1_fast_api_test.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI 
+import uvicorn 
+
+app = FastAPI()
+
+def test_api_upcase(query: str):
+    buff = query.upper() 
+    return {"agent_response": buff}
+
+app.add_api_route(
+    path = "/test/{query}", 
+    endpoint=test_api_upcase,
+    methods = ["GET"]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
