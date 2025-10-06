ea981495a250370274597b4b91ee56c87b6cdeca
Alexandr Cecetov <66001736+alex2mendosa@users.noreply.github.com>
2025-10-06 13:56:55 +0300

    Update 1_fast_api_test.py

--- 1_fast_api_test.py
+++ 1_fast_api_test.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI 
 import uvicorn 
+from pydantic import BaseModel
 
 app = FastAPI()
 
-def test_api_upcase(query: str):
-    buff = query.upper() 
+class query_result( BaseModel ): 
+     query:str 
+
+
+def test_api_upcase(request: query_result):
+    buff = request.query.upper() 
     return {"agent_response": buff}
 
 app.add_api_route(
-    path = "/test/{query}", 
+    path = "/test", 
     endpoint=test_api_upcase,
-    methods = ["GET"]
+    methods = ["POST"]
 )
 
 if __name__ == "__main__":
-
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
